9071637494318da6799c6d93a923819b44082e71
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-06-21 12:35:08 +0600

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "MediCare Connect",
+  description: "Hospital Appointment & Healthcare Management System",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
